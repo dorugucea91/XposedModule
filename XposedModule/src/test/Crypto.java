662be1e62c474fae66d774b8a42eac33da26026d
dorugucea91 <gucea.doru@gmail.com>
2014-06-11 19:03:43 +0300

    unhook methods

--- XposedModule/src/test/Crypto.java
+++ XposedModule/src/test/Crypto.java
@@ -1,20 +1,28 @@
 package test;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Member;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.HashMap;
 import java.util.Map;
+
 import android.annotation.SuppressLint;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources.Unhook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class Crypto implements IXposedHookLoadPackage {
 	@SuppressLint("UseSparseArrays")
 	static Map<Integer, Streams> keys = new HashMap<Integer, Streams>();
+	XC_MethodHook.Unhook uIs;
+	XC_MethodHook.Unhook uOs;
 
 	public void handleLoadPackage(final LoadPackageParam lpparam)
 			throws Throwable {
@@ -23,19 +31,26 @@ public class Crypto implements IXposedHookLoadPackage {
 
 		/* debugging */
 		XposedBridge.log("androidclient package");
-		
+				
 		findAndHookMethod("java.net.Socket", lpparam.classLoader, "connect",
 			SocketAddress.class, new XC_MethodHook() {
 			@Override
 			protected void afterHookedMethod(MethodHookParam param)
-					throws Throwable {
+					throws Throwable {	
+				
+				if (uIs != null) {
+					XposedBridge.unhookMethod(uIs.getHookedMethod(), uIs.getCallback());
+				}
+				if (uOs != null)
+					XposedBridge.unhookMethod(uOs.getHookedMethod(), uOs.getCallback());
+				
 				XposedBridge.log("android client connect");
 				Socket clientSocket = (Socket) param.thisObject;
 
-//				XposedBridge.log("before");
-//				OutputStream os = clientSocket.getOutputStream();
-//				InputStream is = clientSocket.getInputStream();
-//				XposedBridge.log("after");
+				XposedBridge.log("before");
+				OutputStream os = clientSocket.getOutputStream();
+				InputStream is = clientSocket.getInputStream();
+				XposedBridge.log("after");
 
 				/* debugging */
 				XposedBridge.log(Integer
@@ -61,8 +76,8 @@ public class Crypto implements IXposedHookLoadPackage {
 				Streams stream = new Streams(isCrypt, osCrypt);
 				keys.put(Integer.valueOf(clientSocket.getPort()),
 							stream);
-					
-				findAndHookMethod("java.net.Socket", lpparam.classLoader, "getOutputStream", new XC_MethodReplacement() {
+				
+				uOs = findAndHookMethod("java.net.Socket", lpparam.classLoader, "getOutputStream", new XC_MethodReplacement() {
 					@Override
 					protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
 						/* debugging */
@@ -81,7 +96,7 @@ public class Crypto implements IXposedHookLoadPackage {
 					}
 				});
 				
-				findAndHookMethod("java.net.Socket", lpparam.classLoader, "getInputStream", new XC_MethodReplacement() {
+				uIs = findAndHookMethod("java.net.Socket", lpparam.classLoader, "getInputStream", new XC_MethodReplacement() {
 					@Override
 					protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
 						/* debugging */
